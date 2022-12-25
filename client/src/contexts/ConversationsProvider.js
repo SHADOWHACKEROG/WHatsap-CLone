b42b36e13ba0b2dbb58587c4ea2fb89f35f2d910
Hamdan Golandaz <helloworldSimplified@gmail.com>
2022-12-25 00:24:36 -0800

    main

--- client/src/contexts/ConversationsProvider.js
+++ client/src/contexts/ConversationsProvider.js
@@ -46,6 +46,10 @@ export function ConversationsProvider({ id, children }) {
   useEffect(() => {
     if (socket == null) return
 
+    socket.on("connect_error", (err) => {
+      console.log(`connect_error due to ${err.message}`);
+    });
+    
     socket.on('recive-message', addMessageToConversation)
 
     return () => socket.off('recive-message')
--- client/src/contexts/SocketProvider.js
+++ client/src/contexts/SocketProvider.js
@@ -12,7 +12,7 @@ export function SocketProvider({ id, children }) {
     const [socket, setSocket] = useState()
 
         useEffect(() => {
-            const newSocket = io('http://localhost:5000', { query: { id }})
+            const newSocket = io('wss://stingy-zenith-mailbox.glitch.me/', { query: { id } })
             setSocket(newSocket)
 
             return () => newSocket.close()
